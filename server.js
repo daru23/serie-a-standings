724923003041d5da683190f5023f30852c71e83e
Daniela Ruiz <daru015@gmail.com>
2017-10-09 15:53:47 +0200

    home and team details, displaying data taken from a REST API

--- server.js
+++ server.js
@@ -3,3 +3,91 @@
  * Created by Daniela Ruiz
  * 2017-10-09
  */
+
+'use strict'
+
+const Hapi = require('hapi');
+const Request = require('request');
+const Vision = require('vision');
+const Handlebars = require('handlebars');
+const LodashFilter = require('lodash.filter');
+const LodashTake = require('lodash.take');
+
+const server = new Hapi.Server();
+
+server.connection({
+    host: '127.0.0.1',
+    port: 3000
+});
+
+// Register vision for our views
+server.register(Vision, (err) => {
+    server.views({
+        engines: {
+            html: Handlebars
+        },
+        relativeTo: __dirname,
+        path: './views',
+    });
+});
+
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+
+    console.log(`Server running at: ${server.info.uri}`);
+});
+
+// Show teams standings
+server.route(
+    {
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            Request.get('http://api.football-data.org/v1/competitions/438/leagueTable', function (error, response, body) {
+                if (error) {
+                    throw error;
+                }
+
+                const data = JSON.parse(body);
+                reply.view('index', {result: data});
+            });
+        }
+    }
+);
+
+server.route(
+    {
+        method: 'GET',
+        path: '/teams/{id}',
+        handler: function (request, reply) {
+            const teamID = encodeURIComponent(request.params.id);
+
+            Request.get(`http://api.football-data.org/v1/teams/${teamID}`, function (error, response, body) {
+                if (error) {
+                    throw error;
+                }
+
+                const result = JSON.parse(body);
+
+                Request.get(`http://api.football-data.org/v1/teams/${teamID}/fixtures`, function (error, response, body) {
+                    if (error) {
+                        throw error;
+                    }
+
+                    const fixtures = LodashTake(LodashFilter(JSON.parse(body).fixtures, function (match) {
+                        return match.status === 'SCHEDULED';
+                    }), 5);
+
+                    reply.view('team', { result: result, fixtures: fixtures });
+                });
+            });
+        }
+    }
+);
+
+// A simple helper function that extracts team ID from team URL
+Handlebars.registerHelper('teamID', function (teamUrl) {
+    return teamUrl.slice(38);
+});
